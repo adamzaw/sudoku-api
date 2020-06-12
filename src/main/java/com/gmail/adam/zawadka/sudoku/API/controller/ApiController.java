fead1b1e4f207372430e72b87eb8ec9d94ff12d0
Adam Zawadka <zawadka.adam@gmail.com>
2020-06-12 08:49:51 +0200

    saving json format score to db

--- src/main/java/com/gmail/adam/zawadka/sudoku/API/controller/ApiController.java
+++ src/main/java/com/gmail/adam/zawadka/sudoku/API/controller/ApiController.java
@@ -1,13 +1,28 @@
 package com.gmail.adam.zawadka.sudoku.API.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gmail.adam.zawadka.sudoku.API.model.Score;
+import com.gmail.adam.zawadka.sudoku.API.repository.ScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class ApiController {
+
+    @Autowired
+    ScoreRepository scoreRepository;
 
 //    @GetMapping("/getsudoku")
 //    public Sudoku getSudoku(){
 //
 //    }
+
+    @RequestMapping(value = "/score/post", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    @ResponseBody
+    public String setScore(@RequestBody Score score) throws Exception {
+        String test = score.getNick();
+        scoreRepository.save(score);
+        return test;
+    }
 }
--- src/main/java/com/gmail/adam/zawadka/sudoku/API/model/Score.java
+++ src/main/java/com/gmail/adam/zawadka/sudoku/API/model/Score.java
@@ -1,10 +1,13 @@
 package com.gmail.adam.zawadka.sudoku.API.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "score")
-public class Score {
+public class Score implements Serializable {
+
+    private static final long serialVersionUID = -1764970284520387975L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +17,27 @@ public class Score {
     private int level;
     private String nick;
 
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
 }
